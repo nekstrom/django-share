a31a52bc5326fcfe6943a51ce6df06113f32b5db
Sjoerd Arendsen <sjoerd@optixdesigns.com>
2012-05-10 16:09:07 +0200

    Adding template

--- service_links/template_tags/service_links/service_links.py
+++ service_links/template_tags/service_links/service_links.py
@@ -6,6 +6,6 @@ from django.utils.http import urlquote
 register = Library()
 
 @register.inclusion_tag('service_links/links.html', takes_context=True)
-def service_links(context, title, object_or_url, description=""):
+def service_links(context, providers=None):
 	links = {}
     return {'bookmarks':links}
