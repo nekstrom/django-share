d936d021e0befc077548010e8ccfe93ab89b6ab9
Sjoerd Arendsen <sjoerd@optixdesigns.com>
2012-05-11 15:49:58 +0200

    Fix classes

--- share/static/js/share.js
+++ share/static/js/share.js
@@ -4,16 +4,16 @@ $(function() {
     // Facebook
     $.getJSON('http://graph.facebook.com/'+url+'&callback=?',
         function(data) {  
-            $('.share-facebook .button').attr("target", "_blank");   
-            $('.share-facebook .button').attr("href", "http://www.facebook.com/sharer.php?u=" + url); // should make it overidable
-            $('.share-facebook .count').append(data.shares);
+            $('.share-link-facebook .button').attr("target", "_blank");   
+            $('.share-link-facebook .button').attr("href", "http://www.facebook.com/sharer.php?u=" + url); // should make it overidable
+            $('.share-link-facebook .count').append(data.shares);
     });
 
     // Twitter
     $.getJSON('http://urls.api.twitter.com/1/urls/count.json?url='+url+'&callback=?',
         function(data) {
-            $('.share-twitter .button').attr("target", "_blank"); 
-            $('.share-twitter .button').attr("href", "https://twitter.com/share?url=" + url); // should make it overidable
-            $('.share-twitter .count').append(data.count);
+            $('.share-link-twitter .button').attr("target", "_blank"); 
+            $('.share-link-twitter .button').attr("href", "https://twitter.com/share?url=" + url); // should make it overidable
+            $('.share-link-twitter .count').append(data.count);
     });
 });
