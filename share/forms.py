86ab3cec801e40f7421ef3b00b41916ae226d4ea
Sjoerd Arendsen <sjoerd@optixdesigns.com>
2012-05-14 13:42:51 +0200

    Adding redirect option

--- share/forms.py
+++ share/forms.py
@@ -1,7 +1,7 @@
-from django import forms
 from django.forms import *
 
 class EmailForm(forms.Form):
   email_to = EmailField(required=True)
   email_from = EmailField(required=True)
   message = CharField(required=False)
+  next = CharField(required=False, widget=HiddenInput())
--- share/views.py
+++ share/views.py
@@ -2,6 +2,27 @@ from django.views.generic.edit import FormView
 from share.forms import EmailForm
 
 class EmailFormView(FormView):
-	form_class = EmailForm
-	template_name = 'share/email_form.html'
-	pass
+  form_class = EmailForm
+  template_name = 'share/email_form.html'
+
+  def get_initial(self):
+    if self.request.GET:
+      data = self.request.GET.copy()
+    elif self.request.POST: 
+      data = self.request.GET.copy() 
+    
+    next = data.get("next", None)
+
+    initial = self.initial.copy()
+    initial['next'] = next
+
+    return initial
+
+  def get_success_url(self):
+    data = self.request.POST.copy()
+    next = data.get("next", None)
+
+    if next:
+      self.success_url = next
+
+    return super(EmailFormView, self).get_success_url()
