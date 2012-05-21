e0d8d1efc455c406ffaa715a40fcbefae3e00a4b
Ferdi Ferwerda <fferwerda@hub.nl>
2012-05-21 16:39:35 +0200

    Stylesheets and actions for more button

--- share/static/js/share.js
+++ share/static/js/share.js
@@ -16,6 +16,27 @@ $(function() {
             $('.share-link-twitter .button').attr("href", "https://twitter.com/share?url=" + url); // should make it overidable
             $('.share-link-twitter .count').append(data.count);
     });
+
+    // Sliding 'more' actions
+    $('.share-bar a.more').click(function() {
+      if ($("ul.share-bar-more").hasClass('closed')) {
+        $('ul.share-bar-more').removeClass('closed');
+        $('ul.share-bar-more').addClass('open');
+        $('.share-bar').animate({
+          "width": "100%"
+        }), 2000;
+      } else {
+        $('ul.share-bar-more').removeClass('open');
+        $('ul.share-bar-more').addClass('closed');
+        $('.share-bar').animate({
+          "width": "340px"
+        }), 2000;
+      }
+    });    
+
+    // $('.share-bar a.more').click(function() {
+    //   $('ul.share-bar-more').slideToggle('slow');
+    // });
   /*
   ($ "a[data-toggle=modal]").click ->
   target = ($ @).attr('data-target')
