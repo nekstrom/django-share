c89e31bc7c4130b1450f23d364ac02808e9291e2
Sjoerd Arendsen <sjoerd@optixdesigns.com>
2012-05-22 15:20:09 +0200

    Make pinterest custom aswell

--- share/static/js/share.js
+++ share/static/js/share.js
@@ -3,18 +3,22 @@ $(function() {
     
     // Facebook
     $.getJSON('http://graph.facebook.com/'+url+'&callback=?',
-        function(data) {  
-            //$('.share-link-facebook .button').attr("target", "_blank");   
-            //$('.share-link-facebook .button').attr("href", "http://www.facebook.com/sharer.php?u=" + url); // should make it overidable
-            $('.share-facebook-count').append(data.shares);
+      function(data) {  
+          $('.share-facebook-count').text(data.shares);
     });
 
     // Twitter
     $.getJSON('http://urls.api.twitter.com/1/urls/count.json?url='+url+'&callback=?',
-        function(data) {
-            $('.share-link-twitter .button').attr("target", "_blank"); 
-            $('.share-link-twitter .button').attr("href", "https://twitter.com/share?url=" + url); // should make it overidable
-            $('.share-link-twitter .count').append(data.count);
+      function(data) {
+        $('.share-link-twitter .button').attr("target", "_blank"); 
+        $('.share-link-twitter .button').attr("href", "https://twitter.com/share?url=" + url); // should make it overidable
+        $('.share-link-twitter .count').text(data.count);
+    });
+
+    // Pinterest
+    $.getJSON('http://api.pinterest.com/v1/urls/count.json?callback=receiveCount&url=' + url,
+      function(data) {  
+        $('.share-pinterest-count').text(data.shares);
     });
 
     // Sliding 'more' actions
