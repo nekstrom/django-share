32ad3a5e28d4bd6ac0965dc46c47fc86500c490b
Sjoerd Arendsen <sjoerd@optixdesigns.com>
2012-05-22 14:44:41 +0200

    Really send the email

--- share/views.py
+++ share/views.py
@@ -1,5 +1,7 @@
 from django.views.generic.edit import FormView
 from share.forms import EmailForm
+from django.core.mail import send_mail
+from django.utils.html import escape
 
 class EmailFormView(FormView):
   form_class = EmailForm
@@ -10,13 +12,19 @@ class EmailFormView(FormView):
       data = self.request.GET.copy()
     elif self.request.POST: 
       data = self.request.GET.copy() 
-    
+    else:
+      return {}
+
     next = data.get("next", None)
 
     initial = self.initial.copy()
     initial['next'] = next
 
     return initial
+  
+  def form_valid(self, form):
+    send_mail('Someone wants to share something with you!', escape(form.cleaned_data['message']), escape(form.cleaned_data['email_from']), [escape(form.cleaned_data['email_to'])], fail_silently=False)
+    return super(EmailFormView, self).form_valid(form)
 
   def get_success_url(self):
     data = self.request.POST.copy()
