1437452c9df47d980db6eeb89b9c58552674c302
Sjoerd Arendsen <sjoerd@optixdesigns.com>
2012-05-22 10:35:42 +0200

    Fix settings

--- share/settings.py
+++ share/settings.py
@@ -1,8 +1,8 @@
 from django.conf import settings
 
 providers = {
-  'main': {'facebook', 'twitter', 'pinterest', 'googleplus',},
-  'more': {'email', 'print',},
+  'main': ['facebook', 'twitter', 'pinterest', 'googleplus',],
+  'more': ['email', 'print',],
 }
 
 SHARE_PROVIDERS = getattr(settings, 'SHARE_PROVIDERS', providers)
